3cc2f3fe51e3f85f30c4c48333d2288a750a5324
Tran Quang Huy <tranquanghuy12h@gmail.com>
2022-03-04 15:35:45 +0700

    Edit app.js file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
-import BaiTapDungLayout from "./BaiTapDungLayout/BaiTapDungLayout";
-import HandleEvent from "./EventHandler/HandleEvent";
-import LoginForm from "./EventHandler/LoginForm";
-import StyleComponent from "./StyleComponent/StyleComponent";
-import BaiTapChonXe from "./StateDemo/BaiTapChonXe/BaiTapChonXe";
-import BaiTapTangGiamFont from "./StateDemo/BaiTapTangGiamFont/BaiTapTangGiamFont";
-import RenderWithMap from "./RenderWithMap/RenderWithMap";
-import DemoProps from "./Props/DemoProps/DemoProps";
-import BaiTapQLSP from "./Props/BaiTapQLSP/BaiTapQLSP";
-import BaiTapXemChiTiet from "./Props/BaiTapXemChiTiet/BaiTapXemChiTiet";
-import ExerciseCarStore from "./Props/ExerciseCarStore/ExerciseCarStore";
-import DemoPropsChildren from "./Props/DemoPropsChildren/DemoPropsChildren";
-import BaiTapChonXeRedux from "./DemoRedux/BaiTapChonXeRedux/BaiTapChonXeRedux";
+// import BaiTapDungLayout from "./BaiTapDungLayout/BaiTapDungLayout";
+// import HandleEvent from "./EventHandler/HandleEvent";
+// import LoginForm from "./EventHandler/LoginForm";
+// import StyleComponent from "./StyleComponent/StyleComponent";
+// import BaiTapChonXe from "./StateDemo/BaiTapChonXe/BaiTapChonXe";
+// import BaiTapTangGiamFont from "./StateDemo/BaiTapTangGiamFont/BaiTapTangGiamFont";
+// import RenderWithMap from "./RenderWithMap/RenderWithMap";
+// import DemoProps from "./Props/DemoProps/DemoProps";
+// import BaiTapQLSP from "./Props/BaiTapQLSP/BaiTapQLSP";
+// import BaiTapXemChiTiet from "./Props/BaiTapXemChiTiet/BaiTapXemChiTiet";
+// import ExerciseCarStore from "./Props/ExerciseCarStore/ExerciseCarStore";
+// import DemoPropsChildren from "./Props/DemoPropsChildren/DemoPropsChildren";
+// import BaiTapChonXeRedux from "./DemoRedux/BaiTapChonXeRedux/BaiTapChonXeRedux";
 import BaiTapThuKinh from "./Props/BaiTapThuKinh/BaiTapThuKinh";
 
 function App() {
